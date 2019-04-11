b6514aa1a975d6d0e6d082e4c63d9561a10fa85f
Clément Auger <clement.auger@epitech.eu>
2019-04-11 11:24:22 +0200

    test flag2

--- calcul.py
+++ calcul.py
@@ -3,19 +3,34 @@
 from math import pi, sqrt, exp
 import time
 
+def formule_205(i, mu, sigma):
+    tmp = exp(-((i - mu) * (i - mu) / (2 * sigma * sigma)))
+    result = 1 / (sigma * sqrt(2 * pi)) * tmp
+    return result
+
 def mode_1(mu, sigma):
     i = 0
-    tmp = 0.0
     result = 0.0
 
     while (i <= 200):
-        tmp = exp(-((i - mu) * (i - mu) / (2 * sigma * sigma)))
-        result = 1 / (sigma * sqrt(2 * pi)) * tmp
+        result = formule_205(i, mu, sigma)
         print(i, "%.5f" % result)
         i += 1
     return
 
 def mode_2(mu, sigma, IQstr):
+    try:
+        iq = int(IQstr)
+    except:
+        quit(84)
+    
+    i = 0
+    result = 0.0
+
+    while (i < iq):
+        result += formule_205(i, mu, sigma)
+        i += 1
+    print(result * 100)
     return
 
 def mode_3(mu, sigma, IQstr1, IQstr2):
