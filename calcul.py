983e98f590f161f4ec9493628cc77ea6f6fb568f
Clément Auger <clement.auger@epitech.eu>
2019-04-11 10:57:43 +0200

    skeleton done

--- calcul.py
+++ calcul.py
@@ -3,3 +3,11 @@
 from math import exp, factorial
 import time
 
+def mode_1(mu, sigma):
+    return
+
+def mode_2(mu, sigma, IQstr):
+    return
+
+def mode_3(mu, sigma, IQstr1, IQstr2):
+    return
