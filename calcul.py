2f5804555875844202a9cbd4a63a036de203cac8
Clément Auger <clement.auger@epitech.eu>
2019-04-11 11:14:48 +0200

    mod1

--- calcul.py
+++ calcul.py
@@ -1,9 +1,18 @@
 #/bin/python3
 
-from math import exp, factorial
+from math import pi, sqrt, exp
 import time
 
 def mode_1(mu, sigma):
+    i = 0
+    tmp = 0.0
+    result = 0.0
+
+    while (i <= 200):
+        tmp = exp(-((i - mu) * (i - mu) / (2 * sigma * sigma)))
+        result = 1 / (sigma * sqrt(2 * pi)) * tmp
+        print(i, "%.5f" % result)
+        i += 1
     return
 
 def mode_2(mu, sigma, IQstr):
